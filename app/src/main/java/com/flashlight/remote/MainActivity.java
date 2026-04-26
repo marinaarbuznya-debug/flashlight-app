219205aa1a0401664c23fe8214e499bc6d8b7b19
marinaarbuznya-debug <marinaarbuznya@gmail.com>
2026-04-26 20:16:24 +0300

    Update MainActivity.java

--- app/src/main/java/com/flashlight/remote/MainActivity.java
+++ app/src/main/java/com/flashlight/remote/MainActivity.java
@@ -2,34 +2,26 @@ package com.flashlight.remote;
 
 import android.app.Activity;
 import android.os.Bundle;
-import androidx.work.*;
 import com.google.firebase.messaging.FirebaseMessaging;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FirebaseMessaging.getInstance().getToken()
-            .addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    String token = task.getResult();
-                    com.google.firebase.database.FirebaseDatabase.getInstance()
-                        .getReference("token")
-                        .setValue(token);
-                }
-            });
-
-        PeriodicWorkRequest keepAlive = new PeriodicWorkRequest.Builder(
-            KeepAliveWorker.class, 15, TimeUnit.MINUTES)
-            .build();
-
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-            "keepAlive",
-            ExistingPeriodicWorkPolicy.REPLACE,
-            keepAlive
-        );
+        try {
+            FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        String token = task.getResult();
+                        com.google.firebase.database.FirebaseDatabase.getInstance()
+                            .getReference("token")
+                            .setValue(token);
+                    }
+                });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         finish();
     }
